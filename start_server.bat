f158cdacc354f63a390157783a3632861e43646b
murahiro1126 <murahiro1126@gmail.com>
2026-09-23 18:42:47 +0900

    Renderデプロイ用のrequirements.txtを追加

--- start_server.bat
+++ start_server.bat
@@ -9,7 +9,7 @@ if not exist ".venv\Scripts\python.exe" (
   exit /b 1
 )
 
-start "Stock Board Server" "%ComSpec%" /k ""%~dp0.venv\Scripts\python.exe" -m uvicorn main:app --host 127.0.0.1 --port 8000"
+start "Stock Board Server" "%ComSpec%" /k ""%~dp0.venv\Scripts\python.exe" -m uvicorn main:app --host 0.0.0.0 --port 8000"
 timeout /t 2 /nobreak >nul
-start "" "http://127.0.0.1:8000/"
+start "" "http://192.168.0.9:8000/"
 endlocal
